66e91a277ad9cdd16a496ae1f0e85b6cb6ce45a6
Cory Pease <coryrpease@gmail.com>
2024-11-27 20:30:00 -0700

    Add CRD functionality
    Endpoints will simply return request body

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,9 +10,17 @@ tasks = [
     {"id": 3, "task": "Prepare dinner"}
 ]
 
-@app.route('/tasks', methods=['GET'])
+@app.route('/tasks', methods=['GET','OPTIONS'])
 def test_endpoint():
     return jsonify(tasks), 200
+
+@app.route('/tasks', methods=['POST', 'OPTIONS'])
+def create_task():
+    return request.get_json()
+
+@app.route('/tasks', methods=['DELETE', 'OPTIONS'])
+def delete_task():
+    return request.get_json()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
